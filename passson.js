d622048f88162c77d8f0d2e59d4c07120c7f4cbd
Zhibeka-dev <pirnazarovazhibek@gmail.com>
2025-02-14 10:24:36 +0600

    Update passson.js

--- passson.js
+++ passson.js
@@ -1,16 +1,16 @@
 function confirmPwd(){​
 
-	   var pass1=document.getElementById("pass1").value;​
+	   var pass1=document.getElementById("password").value;​
 
-	   var pass2=document.getElementById("pass2").value;​
+	   var pass2=document.getElementById("confirmpassword").value;​
 
 		   var ok=true;​
 
 		   if(pass1!=pass2){​
 
-		   document.getElementById("pass1").style.borderColor="#E34234";​
+		   document.getElementById("password").style.borderColor="#E34234";​
 
-		   document.getElementById("pass2").style.borderColor="#E34234";  ​
+		   document.getElementById("confirmpassword").style.borderColor="#E34234";  ​
 
 			   alert("Passwords do not match"); ​
 
